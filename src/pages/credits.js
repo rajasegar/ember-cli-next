3aad01b80846e677301b8540ecb3bd9d0368d805
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-08-10 18:12:52 +0530

    feat: add lcd widget to credits page

--- src/pages/credits.js
+++ src/pages/credits.js
@@ -6,7 +6,7 @@ const contrib = require('blessed-contrib');
 module.exports = function (screen) {
   const grid = new contrib.grid({ rows: 12, cols: 12, screen: screen });
 
-  grid.set(0, 0, 6, 12, blessed.bigtext, {
+  grid.set(0, 0, 3, 12, blessed.bigtext, {
     parent: screen,
     content: 'ember-cli-next',
     width: '80%',
@@ -15,10 +15,54 @@ module.exports = function (screen) {
     fch: ' ',
     style: {
       fg: 'white',
-      //bg: 'blue',
       bold: true,
     },
   });
+
+  const lcd = grid.set(3, 0, 3, 12, contrib.lcd, {
+    segmentWidth: 0.05, // how wide are the segments in % so 50% = 0.5
+    segmentInterval: 0.11, // spacing between the segments in % so 50% = 0.550% = 0.5
+    strokeWidth: 0.11, // spacing between the segments in % so 50% = 0.5
+    elementSpacing: 4, // spacing between each element
+    elementPadding: 2,
+    elements: 14,
+  });
+
+  let temp = '';
+  let counter = 0;
+  setInterval(function () {
+    const colors = ['green', 'magenta', 'cyan', 'red', 'blue'];
+    const text = [
+      'E',
+      'M',
+      'B',
+      'E',
+      'R',
+      '-',
+      'C',
+      'L',
+      'I',
+      '-',
+      'N',
+      'E',
+      'X',
+      'T',
+    ];
+
+    const value = Math.round(Math.random() * 1000);
+    //lcd.setDisplay('EMBER-CLI-NEXT');
+
+    temp = temp + text[counter++];
+    if (counter > 14) {
+      counter = 0;
+      temp = '';
+    }
+    lcd.setDisplay(temp);
+    lcd.setOptions({
+      color: colors[value % 5],
+    });
+    screen.render();
+  }, 1000);
 
   const credits = `
   Thanks to this awesome libraries/projects without which ember-cli-next wouldn't be possible.
@@ -34,6 +78,7 @@ module.exports = function (screen) {
 
   grid.set(6, 0, 6, 12, blessed.box, {
     content: credits,
+    label: 'Credits',
   });
 
   screen.render();
